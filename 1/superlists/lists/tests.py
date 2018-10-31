8a551438b47f68fe9a3a5f78b551d04c42bd91f6
Crossroadsman <crossroadsman@gmail.com>
2018-10-31 15:12:40 -0600

    Write failing test re content of home page html

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
+from django.http import HttpRequest
 from django.test import TestCase
+
 from lists.views import home_page
 
 
@@ -10,3 +12,23 @@ class HomePageTest(TestCase):
         # https://docs.djangoproject.com/en/2.1/ref/urlresolvers/#resolve
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        # the view function returns an HttpResponse object
+        response = home_page(request)
+
+        # HttpResponse objects have a .content attribute that
+        # contains the 'raw bytes, the ones and zeroes that would be sent
+        # down the wire to the user's browser'
+        html = response.content.decode('utf8')
+
+        # We determine that the HTML is (at least superficially) valid
+        # if it:
+        # - starts with the required doctype
+        # - contains somewhere the correct title (remember the functional
+        #                                         test)
+        # - ends with a closing html tag
+        self.assertTrue(html.startswith('<!doctype html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
