8192416551c77be9baa4ef4d09d1b2ae8369bc5c
Crossroadsman <crossroadsman@gmail.com>
2018-10-31 14:47:51 +0000

    Create test to verify test suite is being picked up by test runner

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SmokeTest(TestCase):
+    # This is a deliberately silly failing test to verify that the 
+    # test runner is actually seeing this TestCase
+    # (the django test runner is invoked using
+    #     $ python3 manage.py test
+    # )
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 3)
