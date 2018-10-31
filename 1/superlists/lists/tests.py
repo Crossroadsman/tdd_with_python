7b3b36b0623406f5c85e9c04f2c7a85b6597bf86
Crossroadsman <crossroadsman@gmail.com>
2018-10-31 15:57:54 -0600

    Implement template testing rather than testing constants

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -1,5 +1,4 @@
 from django.urls import resolve
-from django.http import HttpRequest
 from django.test import TestCase
 
 from lists.views import home_page
@@ -14,10 +13,18 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        # the view function returns an HttpResponse object
-        response = home_page(request)
+        # instead of manually creating an HttpRequest object (as we did
+        # in the earlier version of this test, we can use self.client.get
+        # (part of the Django extensions to TestCase)
+        response = self.client.get('/')  # the view returns a HttpResponse
+                                         # object
 
+        # We don't need to test constants, we can just test that the right
+        # template gets loaded (the functional test will still prove that
+        # the right content is shown to the user).
+        # We'll leave the constant-checking here for now, to make sure
+        # everything is working the way we think it is.
+        
         # HttpResponse objects have a .content attribute that
         # contains the 'raw bytes, the ones and zeroes that would be sent
         # down the wire to the user's browser'
@@ -32,3 +39,5 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<!doctype html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'lists/wrong.html')
