d3c99a2b14c0309d5989da79d74b41a90873bd78
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:34:45 -0600

    Improve error message in functional tests (using new f-strings!)

--- 1/superlists/functional_tests.py
+++ 1/superlists/functional_tests.py
@@ -75,13 +75,13 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(1)  # seconds
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"  # f-strings are new in Python 3.6
         )
 
         # There is still a text box inviting her to add another item. She
