54d587fe2daeaf692e7118de78915215310ad91a
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:59:34 -0600

    Refactor test elements that get rows from table

--- 1/superlists/functional_tests.py
+++ 1/superlists/functional_tests.py
@@ -40,12 +40,23 @@ export DISPLAY=:10
 """
 class NewVisitorTest(unittest.TestCase):
 
+    # setUp and tearDown
+    # ------------------
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
+    # helper methods
+    # --------------
+    def get_table_row_texts(self):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        return [row.text for row in rows]
+
+    # Tests
+    # -----
     def test_can_start_a_list_and_retrieve_it_later(self):
         """TODO 20181025: This is a huge test that tests a whole bunch of 
         things. Is this typical for functional tests?
@@ -82,9 +93,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)  # seconds
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', self.get_table_row_texts())
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
@@ -94,11 +103,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        row_texts = [row.text for row in rows]
+        row_texts = self.get_table_row_texts()
         self.assertIn('1: Buy peacock feathers', row_texts)
-        self.assertIn('2: Use peacock feathers to make a fly')
+        self.assertIn('2: Use peacock feathers to make a fly', row_texts)
 
         # Alice wants the site to remember he list. She sees that the site
         # has generated a unique URL for her, together with explanatory text
