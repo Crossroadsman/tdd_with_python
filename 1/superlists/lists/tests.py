16b521e246c9a4df71e287e26b371e31dd36d089
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:01:21 -0600

    Create new test to check POST data

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -6,7 +6,7 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_home_template(self):
         # instead of manually creating an HttpRequest object (as we did
         # in the earlier version of this test, we can use self.client.get
         # (part of the Django extensions to TestCase)
@@ -17,3 +17,28 @@ class HomePageTest(TestCase):
         # template gets loaded (the functional test will still prove that
         # the right content is shown to the user).
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_home_page_can_save_POST_request(self):
+        item_text = "A new list item"
+
+        # self.client.post takes a `data` argument that contains a
+        # dictionary of the form data, where the key is the
+        # form's `name` attribute and the value is whatever we want
+        # to supply as a value to that form input.
+        response = self.client.post('/', data={'item_text': item_text})
+
+        # The HttpResponse object has a `content` attribute which is of
+        # type byte sequence (`bytes`)
+        # `bytes` has a `decode(encoding="utf-8", errors="strict)` method
+        # note that content is utf-8 encoded so we can use the defaults
+        # for `decode`
+        response_text = response.content.decode()
+        # The following print statements are just to visually compare
+        # the byte sequence and the python string
+        print("----- Here is what the (utf-8 encoded) byte sequence looks like -----")
+        print(response.content)
+        print("----- End bytestring -----")
+        print("----- Here is what the Python string looks like -----")
+        print(response_text)
+        print("----- End Python string -----")
+        self.assertIn(item_text, response_text)
