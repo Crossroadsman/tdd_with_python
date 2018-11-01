427ba1491783b1e1a9d894f775f03e5b4899de7d
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:51:38 -0600

    Add additional testing to functional_tests to catch 'cheat' solution to prior test

--- 1/superlists/functional_tests.py
+++ 1/superlists/functional_tests.py
@@ -49,6 +49,11 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """TODO 20181025: This is a huge test that tests a whole bunch of 
         things. Is this typical for functional tests?
+
+        A 20181101: It seems necessary for a functional (=integration)
+        test to be long and include a bunch of things since the single
+        piece of functionality we are testing is the user's ability to
+        complete a task, which comprises a sequence of user interactions.
         """
 
         # Alice has heard about a cool new online to-do app. She goes to 
@@ -83,13 +88,22 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        row_texts = [row.text for row in rows]
+        self.assertIn('1: Buy peacock feathers', row_texts)
+        self.assertIn('2: Use peacock feathers to make a fly')
 
         # Alice wants the site to remember he list. She sees that the site
         # has generated a unique URL for her, together with explanatory text
         # to that effect.
+        self.fail('Finish the test!')
 
         # She visits the URL: her to-do list is still there.
 
