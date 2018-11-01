58c17f35fdd12fb4d69bb460b483fea9df7a76ea
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:20:40 -0600

    Add new test to verify that home_page uses the template both for GET and POST

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -5,8 +5,11 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+    def setUp(self):
+        self.item_text = 'A new list item'
+        self.post_data = {'item_text': self.item_text}
 
-    def test_home_page_uses_home_template(self):
+    def test_home_page_GET_uses_home_template(self):
         # instead of manually creating an HttpRequest object (as we did
         # in the earlier version of this test, we can use self.client.get
         # (part of the Django extensions to TestCase)
@@ -18,14 +21,17 @@ class HomePageTest(TestCase):
         # the right content is shown to the user).
         self.assertTemplateUsed(response, 'lists/home.html')
 
+    def test_home_page_POST_uses_home_template(self):
+        response = self.client.post('/', data=self.post_data)
+        self.assertTemplateUsed(response, 'lists/home.html')
+
     def test_home_page_can_save_POST_request(self):
-        item_text = "A new list item"
 
         # self.client.post takes a `data` argument that contains a
         # dictionary of the form data, where the key is the
         # form's `name` attribute and the value is whatever we want
         # to supply as a value to that form input.
-        response = self.client.post('/', data={'item_text': item_text})
+        response = self.client.post('/', data=self.post_data)
 
         # The HttpResponse object has a `content` attribute which is of
         # type byte sequence (`bytes`)
@@ -41,4 +47,4 @@ class HomePageTest(TestCase):
         print("----- Here is what the Python string looks like -----")
         print(response_text)
         print("----- End Python string -----")
-        self.assertIn(item_text, response_text)
+        self.assertIn(self.item_text, response_text)
