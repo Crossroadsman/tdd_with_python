f25510ef351f1828fd76b450a6c4ceb9d2e602f4
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 14:04:01 -0600

    Create code to pass test

--- 1/superlists/lists/views.py
+++ 1/superlists/lists/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    # GET
     template = 'lists/home.html'
     return render(request, template)
