714346153ca6df9486f418416f60e3f8fd5adb12
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 19:19:13 -0600

    Address homepage tests that checks for DB entries

--- 1/superlists/lists/views.py
+++ 1/superlists/lists/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Item
 
 
 def home_page(request):
     template = 'lists/home.html'
     if request.method == 'POST':
-        context = {'new_item_text': request.POST['item_text']}
+        item = Item()
+        item.text = request.POST['item_text']
+        item.save()
+
+        context = {'new_item_text': item.text,}
     else:  # GET
         context = {}
     return render(request, template, context)
