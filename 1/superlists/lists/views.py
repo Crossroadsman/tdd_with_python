feecfec1afcb400db14c58c3bef2022ba9b02312
Crossroadsman <crossroadsman@gmail.com>
2018-11-01 19:27:00 -0600

    Refactor

--- 1/superlists/lists/views.py
+++ 1/superlists/lists/views.py
@@ -6,11 +6,10 @@ from .models import Item
 def home_page(request):
     template = 'lists/home.html'
     if request.method == 'POST':
-        item = Item()
-        item.text = request.POST['item_text']
-        item.save()
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
 
-        context = {'new_item_text': item.text,}
+        context = {'new_item_text': new_item_text,}
     else:  # GET
         context = {}
     return render(request, template, context)
