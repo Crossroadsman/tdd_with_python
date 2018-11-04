3660808aed2666a236faeb07cd8311199d2325ce
Crossroadsman <crossroadsman@gmail.com>
2018-11-04 15:06:33 -0700

    Start implementing List model

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -68,8 +68,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='item 1: puppies')
-        Item.objects.create(text='item 2: pens')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1: puppies', list=list_)
+        Item.objects.create(text='item 2: pens', list=list_)
  
         response = self.client.get('/lists/the-only-list-in-the-world/')
         # The HttpResponse object has a `content` attribute which is of
