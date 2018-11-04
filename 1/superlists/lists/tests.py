9b9d9564726ea034548f828880ed91a96fa468d2
Crossroadsman <crossroadsman@gmail.com>
2018-11-04 14:44:35 -0700

    Create new test for List

--- 1/superlists/lists/tests.py
+++ 1/superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -25,7 +25,7 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saved_items_can_be_retrieved(self):
         """Ensure saaved items are persisted and can be retrieved.
@@ -34,13 +34,21 @@ class ItemModelTest(TestCase):
         It should more properly be described as an integration test.
         We will return to this later.
         """
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -48,7 +56,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
