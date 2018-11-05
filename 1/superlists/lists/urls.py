f42aba1d52233e1940e707514dcb3fb09e034544
Crossroadsman <crossroadsman@gmail.com>
2018-11-04 21:19:18 -0700

    Refactor urls.py

---
+++ 1/superlists/lists/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from . import views
+
+# Note that with our URLs we are using the following convention:
+# - urls that modify the database ('action' urls): no trailing slash
+# - urls that do not modify the DB: trailing slash
+urlpatterns = [
+    re_path(r'^new$', views.new_list, name='new_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+]
