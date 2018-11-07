2211c786888613e733a2de803dd72a12a1a5740c
Crossroadsman <crossroadsman@gmail.com>
2018-11-07 15:34:45 -0700

    Add instructions for installing geckodriver
    (this might be included in some distributions of firefox but seems not
    in the version that is part of Ubuntu's 18.04 apt list as of 20181107)

--- 1/superlists/functional_tests/tests.py
+++ 1/superlists/functional_tests/tests.py
@@ -44,16 +44,30 @@ functional tests confirm the code is doing the right things.
 """Installation Notes
    ------------------
 To enable firefox to run on a headless Ubuntu server we need to:
+- install geckodriver (mozilla/geckodriver on github)
+  ```console
+  # <EITHER>
+  wget <url> -O geckodriver.tar.gz
+  # <OR>
+  curl -L <url> -o geckodriver.tar.gz
+  # <THEN>
+  mkdir ~/bin
+  tar -xvf geckodriver.tar.gz -C ~/bin
+  # <THEN MAYBE (SEE COMMENTS)>
+  chmod +x ~/bin/geckodriver  # if doesn't already have execute permission
+  echo PATH=$PATH:~/bin >> ~/.profile  # if ~/bin isn't already in path
+  . ~/.profile
+  ```
 - install firefox
 - install xvfb (the X windows virtual framebuffer)
 - Run xvfb in the background (of the session running the tests)
-```console
-Xvfb :10 -ac &
-```
+  ```console
+  Xvfb :10 -ac &
+  ```
 - set the DISPLAY variable
-```console
-export DISPLAY=:10
-```
+  ```console
+  export DISPLAY=:10
+  ```
 """
 
 
