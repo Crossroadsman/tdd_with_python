83ad7ace6e71f7c20aa756697e92431f03299393
CrossroadsMan <crossroadsman@gmail.com>
2018-11-16 14:59:14 -0700

    namespace initialize() and wrap in onload boilerplate

--- lists/static/lists/js/list.js
+++ lists/static/lists/js/list.js
@@ -1,10 +1,17 @@
+/* create namespace 
+   we are explicitly declaring Superlists to be a property of the `window`
+   global, giving it a name that no-one else in our project-space is likely
+   to use.
+   Then we will make `initialize` an attribute of that namespace object
+*/
+window.Superlists = {};
 /* we create the `initialize` function so that we can determine when
    the event listener is created. This is important because the `fixture`
    div in qunit replaces the contents in the page every time a new test
    is run and thus the element to which the listener was attached gets
    removed
 */
-const initialize = function () {
+window.Superlists.initialize = function () {
 
     $('input[name="text"]').on('keypress', function () {
         $('.has-error').hide();
