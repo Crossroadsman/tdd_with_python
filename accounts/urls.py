4b1b9ada78373bd7c3cb5f6470b86847fbbedc79
Crossroadsman <crossroadsman@gmail.com>
2018-11-26 14:29:26 -0700

    Start implementing mock

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from . import views
+
+# Note that with our URLs we are using the following convention:
+# - urls that modify the database ('action' urls): no trailing slash
+# - urls that do not modify the DB: trailing slash
+urlpatterns = [
+    re_path(r'^send_login_email',
+        views.send_login_email,
+        name='send_login_email'),
+]
