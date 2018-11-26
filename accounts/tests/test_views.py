41e3d6874c4e693a8869c8a7fe6775e4446f84aa
Crossroadsman <crossroadsman@gmail.com>
2018-11-26 14:40:18 -0700

    Replace manual mock with @patch

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 import accounts.views
@@ -11,25 +13,21 @@ class SendLoginEmailViewTest(TestCase):
             data={'email': 'alice@example.com'})
         self.assertRedirects(response, '/')
 
-    def test_POST_sends_email_to_specified_address(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        # Here we are monkeypatching accounts.views by replacing its
-        # default send_mail() with our fake_send_mail()
-        accounts.views.send_mail = fake_send_mail
+    # @patch takes the name of the object to patch, it then replaces
+    # that object with a MagicMock version for the duration of this
+    # test.
+    # It also passes the mock object as an argument to the test,
+    # (we can call this argument anything we like but convention is to
+    # use `mock_<name_of_original_object>`)
+    @patch('accounts.views.send_mail')
+    def test_POST_sends_email_to_specified_address(self, mock_send_mail):
 
         self.client.post('/accounts/send_login_email', data={
             'email': 'alice@example.com'
         })
 
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link for Superlists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['alice@example.com'])
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['alice@example.com'])
